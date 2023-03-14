aa8e1f5ffa912e73a088530fc3b39a63a5a5c094
Jfausey <jfausey@CORP-JFAU.lan>
2023-03-14 02:56:21 -0400

    Fixing playwright test.

--- tests/playwright.spec.ts
+++ tests/playwright.spec.ts
@@ -7,7 +7,7 @@ test.describe("API Test #1 with Playwright", () => {
         const response = await request.get(`${baseurl}/dress`);
         expect(response.status()).toBe(200);
         const responseBody = JSON.parse(await response.text());
-        expect(responseBody.hits[0].productName).toBe("Modern Dress Shirt");
-        expect(responseBody.hits[0].price).toBe(135);
+        expect(responseBody.hits[0].productName).toBe("Floral Dress");
+        expect(responseBody.hits[0].price).toBe(129);
     });
 });
